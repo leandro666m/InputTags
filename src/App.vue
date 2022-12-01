0dcab853e1d4f79544e0526670eff53aa4a567db
Leandro J Martinez <leandro666m@gmail.com>
2022-12-01 13:00:33 -0300

    creacion del proyecto

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import InputTag from "./components/inputTag.vue";
+  
+</script>
+
+
+
+<template>
+<InputTag />
+
+</template>
+
+
+
+<style scoped>
+
+</style>
---
+++ src/components/InputTag.vue
@@ -0,0 +1,29 @@
+<script>
+    export default {
+        data(){
+            return{
+                currentValue: "",
+                tags: [],
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="inputTag">
+    <div class="tags">  
+        <div class="tag" v-for="(tag, index) in tags" :key="index">
+        
+        </div>
+    
+      </div>
+    <input type="text">
+        
+    </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
