332b76b8cc11ba22aff70a5d43070639244633a0
Leandro J Martinez <leandro666m@gmail.com>
2022-12-02 13:01:31 -0300

    input

--- src/components/InputTag.vue
+++ src/components/InputTag.vue
@@ -1,23 +1,39 @@
 <script>
     export default {
         data(){
-            return{
-                currentValue: "",
-                tags: [],
+            return{  currentValue: "", tags: [] };
+        },
+
+        methods:{
+            handleKeyDown(e){
+                if( e.key === "Enter" && this.currentValue !== " " ){
+                    this.tags.push( this.currentValue );
+                }
+            },
+
+            handleSubmit(){
+                if(  this.currentValue !== " " ){
+                    this.tags.push( this.currentValue );
+                    this.currentValue = "";
+                }
             }
         }
-    }
+    
+    };
+
 </script>
 
 <template>
     <div class="inputTag">
     <div class="tags">  
         <div class="tag" v-for="(tag, index) in tags" :key="index">
-        
+            {{tag}}
         </div>
-    
-      </div>
-    <input type="text">
+    </div>
+
+<form @submit.prevent="handleSubmit">
+    <input type="text" v-model="currentValue" >
+</form>
         
     </div>
 
